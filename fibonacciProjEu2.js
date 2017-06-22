d42747e241e7e8f5e1c56625738c5a9b116cdf95
Mitch Lee <Mitch@mitchlee.org>
2017-06-22 10:20:41 -0400

    Update fibonacciProjEu2.js

--- fibonacciProjEu2.js
+++ fibonacciProjEu2.js
@@ -1,6 +1,8 @@
 var fibo = function (a, b, c) {
-  for (var i = 0; i <= 4000000; i = c)
-  if (c <= 4000000) {
+  console.log(0);
+  console.log(1);
+  for (var i = 0; i < 4000000; i = c)
+  if (c < 4000000) {
     c = a + b;
     a = b;
     b = c;
