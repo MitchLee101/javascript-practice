f70a1f312b4a8f92cd61bf9c408c19ae4d6ddcf5
Mitch Lee <Mitch@mitchlee.org>
2017-06-22 11:00:44 -0400

    Update projEu2-fibonacci.js

--- projEu2-fibonacci.js
+++ projEu2-fibonacci.js
@@ -1,18 +1,19 @@
 var fibo = function (a, b, c) {
+  var fiboSeq = [];
   var evenPlus = 0;
   var evens = [];
-  console.log("Fibonocci Sequence:")
   for (var i = 0; i < 3000000; i = c)
   if (c < 3000000) {
     c = a + b;
     a = b;
     b = c;
-    console.log(c)
+    fiboSeq.push(c)
     if (c % 2 == 0) {
     evens.push(c);
     evenPlus += c;
     }
   }
+  console.log("Fibonocci Sequence: " + fiboSeq);
   console.log("Even Numbers: " + evens);
   return "The sum of all evens are: " + evenPlus;
 };
