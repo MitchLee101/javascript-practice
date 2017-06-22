f07890544208a7c9bd28cadbeecb6e3c2917c72b
Mitch Lee <Mitch@mitchlee.org>
2017-06-22 10:45:33 -0400

    Update and rename fibonacci.js to projEu2-fibonacci.js

---
+++ projEu2-fibonacci.js
@@ -0,0 +1,18 @@
+var fibo = function (a, b, c) {
+  var evenPlus = 0;
+  var evens = [];
+  for (var i = 0; i < 3000000; i = c)
+  if (c < 3000000) {
+    c = a + b;
+    a = b;
+    b = c;
+    console.log(c)
+    if (c % 2 == 0) {
+    evens.push(c);
+    evenPlus += c;
+    }
+  }
+  console.log("Even Numbers: " + evens);
+  return evenPlus;
+};
+fibo (0, 1, 1);
