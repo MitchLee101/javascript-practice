80dcc6c021cf623f87d6cb5897374af8a99ccd9b
Mitch Lee <Mitch@mitchlee.org>
2017-07-21 14:42:58 -0400

    Update ProjectEu1.js
    Updated for codewars

--- ProjectEu1.js
+++ ProjectEu1.js
@@ -1,11 +1,8 @@
-var projEu = function () {
-var sum = 0;
-for (var i = 0; i < 1000; i++) {
-  if (i%3 == 0 || i%5 == 0) {
-    sum += i;
-    }
+function solution(number){
+  var sum = 0;
+  for (var i = 0; i < number; i++)
+  if (i%3 === 0 || i%5 === 0) {
+  sum += i;
   }
   return sum;
-};
-
-projEu();
+}
